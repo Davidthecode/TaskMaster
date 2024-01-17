c13253f240fdc5b6783c0f0c2390726216b99537
David Ajibola <ajiboladavid0963@gmail.com>
2024-01-17 13:17:04 +0100

    mad echanges

--- src/app/components/homepage/taskmasterImg.tsx
+++ src/app/components/homepage/taskmasterImg.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import taskmasterImage from '../../../../public/taskmaster.png';
+import tmImage from "../../../../public/tmImage.png";
 
 export default function TaskmasterImg() {
     return (
-        <div className="flex justify-center pt-20 pb-10 bg-[#EEEBEA]">
-            <div className="shadow-md rounded-lg relative w-[60%] h-[30rem] xs:w-[85%] xs:h-[20rem]">
-                <Image src={taskmasterImage} alt="image" fill={true}/>
+        <div className="flex justify-center pt-20 pb-10 bg-[#EEEBEA] relative">
+            <div className="shadow-md rounded-lg  w-[63%] h-[30rem] xs:w-[85%] xs:h-[20rem]">
+                <Image src={tmImage} alt="image" className="rounded-lg w-full h-full z-50" />
             </div>
         </div>
     )
