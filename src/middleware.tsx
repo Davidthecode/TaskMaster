243efc24dad691915160b94dc13d97759d468978
David Ajibola <ajiboladavid0963@gmail.com>
2024-02-18 13:40:52 +0100

    made changes

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,10 +3,11 @@ import { NextResponse, NextRequest } from 'next/server';
 export default function middleware(request: NextRequest) {
     let loggedin = request.cookies.get("token");
     const { pathname } = request.nextUrl;
+    const links = ["/", "/signup", "/login", "/forgotpassword"];
 
     if (loggedin && (pathname === "/signup" || pathname === "/login")) {
         return NextResponse.redirect(new URL("/home", request.url));
-    } else if (!loggedin && pathname !== "/" && pathname !== "/signup" && pathname !== "/login" && pathname !== "/forgotpassword") {
+    } else if (!loggedin && links.includes(pathname)) {
         return NextResponse.redirect(new URL("/login", request.url));
     };
 };
