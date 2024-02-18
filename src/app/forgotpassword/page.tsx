03aa2d3530dd2840050052bf5e089a3037c351fd
David Ajibola <ajiboladavid0963@gmail.com>
2024-02-18 14:20:06 +0100

    made changes

--- src/app/forgotpassword/page.tsx
+++ src/app/forgotpassword/page.tsx
@@ -1,9 +1,30 @@
 "use client";
 
+import { useState } from "react";
 import taskmasterImage from "../../../public/taskmasterImage.png";
 import Image from "next/image";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../firebase/firebase-config";
+import toast from "react-hot-toast";
 
 export default function ForgotPassword() {
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleForgotPassword = async () => {
+        try {
+            setLoading(true);
+            await sendPasswordResetEmail(auth, email);
+            setMessage("Password reset email sent. Check your inbox");
+            setLoading(false);
+            toast.success("Password reset email sent. Check your inbox");
+        } catch (error) {
+            console.error('error');
+            setMessage("error reseting email");
+        };
+    };
+
     return (
         <section className="flex flex-col items-center">
             <div className="mt-[6rem] flex flex-col items-center">
@@ -11,15 +32,21 @@ export default function ForgotPassword() {
                 <h1 className="text-2xl pt-6 opacity-50">Reset your password</h1>
             </div>
 
-            <div className="bg-gray-200 h-fit w-[30%] rounded-md pt-6 pb-10 px-3 mt-8">
+            <div className="bg-[#F5F2F1] h-fit w-[30%] rounded-md pt-6 pb-10 px-3 mt-8">
                 <p>Enter your user account&apos;s verified email address and we will send you a password reset link.</p>
                 <input
                     type="email"
                     placeholder="Enter your email address"
                     className="w-full rounded-sm px-3 mt-3 h-8 outline-blue-500 border-none"
-
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
-                <button className="mt-20 w-full text-center rounded-md bg-[#426DC6] text-white py-1">send email</button>
+                {loading ? (
+                    <h1>Loading...</h1>
+                ) : (
+                    <button className="mt-20 w-full text-center rounded-md bg-[#426DC6] text-white py-1" onClick={handleForgotPassword}>send email</button>
+                )}
+                {message}
             </div>
         </section>
     );
--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,11 +3,10 @@ import { NextResponse, NextRequest } from 'next/server';
 export default function middleware(request: NextRequest) {
     let loggedin = request.cookies.get("token");
     const { pathname } = request.nextUrl;
-    const links = ["/", "/signup", "/login", "/forgotpassword"];
 
     if (loggedin && (pathname === "/signup" || pathname === "/login")) {
         return NextResponse.redirect(new URL("/home", request.url));
-    } else if (!loggedin && links.includes(pathname)) {
+    } else if (!loggedin && pathname !== "/" && pathname !== "/signup" && pathname !== "/login" && pathname !== "/forgotpassword") {
         return NextResponse.redirect(new URL("/login", request.url));
     };
 };
