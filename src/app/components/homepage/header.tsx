9ce1c9576d84ee445175844ec310402bb0249b9f
David Ajibola <ajiboladavid0963@gmail.com>
2024-02-21 11:35:25 +0100

    made changes

--- src/app/components/homepage/header.tsx
+++ src/app/components/homepage/header.tsx
@@ -32,5 +32,5 @@ export default function Header() {
                 </section>
             </div>
         </section>
-    )
-}
+    );
+};
--- src/app/components/homepage/homepage.tsx
+++ src/app/components/homepage/homepage.tsx
@@ -5,13 +5,14 @@ import Link from "next/link";
 import tmList from "../../../../public/tmlist.png";
 import tmBoard from "../../../../public/tmboard.png";
 import tmCollab from "../../../../public/tmcollab.png";
+import mobileList from "../../../../public/mobileList.png";
 
 export default function Homepage() {
     return (
         <section className="pt-20">
-            <div className="px-[8%] pb-10">
+            <div className="px-[8%] mobile:px- pb-10">
                 <div className="flex justify-center">
-                    <div className="w-[80%] h-[30rem]">
+                    <div className="w-[80%] h-[30rem] largeScreen:h-[24rem] largeTablet:w-[85%] smallTablet:min-w-[100%] largeTablet:h-[22rem] smallTablet:h-[25rem] smallTablet:object-cover">
                         <Image src={taskmasterHomeImage} alt="image" className="w-full h-full rounded-md" />
                     </div>
                 </div>
@@ -22,41 +23,49 @@ export default function Homepage() {
                     </div>
                 </div>
                 <div className="pt-24">
-                    <div className="pb-[7rem] flex items-start">
-                        <div className="w-[60%] pr-4">
+                    {/* Lists */}
+                    <div className="pb-[7rem] flex items-start largeTablet:flex-col smallTablet:flex-col mobile:flex-col largeTablet:space-y-6 smallTablet:space-y-6">
+                        <div className="w-[60%] pr-4 mobile:w-[100%] mobile:pb-6">
                             <h2 className="font-semibold text-2xl">Lists</h2>
                             <p className="text-lg pt-3">The differeent stages of a task. Start as simple as To Do , In progress or Completed. There&apos;s no wrong way to Taskmaster</p>
                         </div>
-                        <div className="w-full h-[25rem]">
-                            <Image src={tmList} alt="image" className="w-full h-full rounded-md" />
+                        <div className="mobile:hidden w-full h-[25rem] largeScreen:h-[20rem] largeTablet:w-[85%] smallTablet:min-w-[100%] largeTablet:h-[22rem] smallTablet:h-[25rem] smallTablet:object-cover">
+                            <Image src={tmList} alt="image" className="w-full h-full rounded-md object-fit" />
+                        </div>
+                        <div className="hidden mobile:block w-full h-[20rem]">
+                            <Image src={mobileList} alt="image" className="w-full h-full object-contain" />
                         </div>
                     </div>
-                    <div className="flex items-start">
-                        <div className="w-full h-[25rem]">
+                    {/* Boards */}
+                    <div className="flex items-start largeTablet:flex-col-reverse smallTablet:flex-col-reverse mobile:flex-col largeTablet:space-y-6 smallTablet:space-y-6">
+                        <div className="mobile:hidden w-full h-[25rem] largeScreen:h-[22rem] largeTablet:w-[85%] smallTablet:min-w-[100%] largeTablet:h-[24rem] smallTablet:h-[25rem] smallTablet:object-cover">
                             <Image src={tmBoard} alt="image" className="w-full h-full rounded-md" />
                         </div>
-                        <div className="w-[60%] pl-4">
+                        <div className="hidden mobile:block w-full h-[20rem]">
+                            <Image src={mobileList} alt="image" className="w-full h-full object-contain" />
+                        </div>
+                        <div className="w-[60%] pl-4 mobile:w-[100%] largeTablet:pb-6 largeTablet:pl-0 smallTablet:pb-6 smallTablet:pl-0">
                             <h2 className="font-semibold text-2xl">Boards</h2>
                             <p className="text-lg pt-3">Taskmaster boards keep tasks organized and work moving foward. In a glance, see everything from &quot;things to do&quot; to &quot;aww yeah, we did it!&quot; </p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="bg-white py-16">
+            <div className="bg-white py-16 px-[8%]">
                 <div className="flex flex-col items-center">
-                    <div className="flex flex-col items-center ">
-                        <h1 className="text-4xl font-semibold">Set Goals</h1>
-                        <div className="w-[40%] pt-4">
-                            <p className="text-center text-lg">Start each day without any surprises. Whether scheduling an editorial calendar or staying on top of to-dos, Calendar view is like a crystal ball giving you a clear vision of what work lies ahead.</p>
+                    <div className="flex flex-col largeTablet:">
+                        <h1 className="text-4xl text-center font-semibold">Set Goals</h1>
+                        <div className="pt-4 flex justify-center">
+                            <p className="text-center text-lg w-[70%]">Start each day without any surprises. Whether scheduling an editorial calendar or staying on top of to-dos, Calendar view is like a crystal ball giving you a clear vision of what work lies ahead.</p>
                         </div>
                     </div>
-                    <div className="w-[55%] h-[25rem] pt-12">
+                    <div className="w-[75%] h-[25rem] largeScreen:h-[22rem] largeTablet:w-[85%] smallTablet:min-w-[100%] largeTablet:h-[24rem] smallTablet:h-[25rem] smallTablet:object-cover  pt-12">
                         <Image src={goals} alt="image" className="w-full h-full rounded-md" />
                     </div>
                 </div>
             </div>
 
-            <div className="py-16">
+            <div className="py-16 px-[8%]">
                 <div className="flex flex-col items-center">
                     <div className="flex flex-col items-center ">
                         <h1 className="text-4xl font-semibold">Collaborate effectively</h1>
@@ -64,7 +73,7 @@ export default function Homepage() {
                             <p className="text-center text-lg">Enhance collaboration with teammates through seamless communication, real-time updates, and a centralized hub for all your tasks.</p>
                         </div>
                     </div>
-                    <div className="w-[55%] h-[25rem] pt-12">
+                    <div className="w-[75%] h-[25rem] largeScreen:h-[22rem] largeTablet:w-[85%] smallTablet:min-w-[100%] largeTablet:h-[24rem] smallTablet:h-[25rem] smallTablet:object-cover pt-12">
                         <Image src={tmCollab} alt="image" className="w-full h-full rounded-md" />
                     </div>
                 </div>
